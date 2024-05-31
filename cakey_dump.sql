c46575a91b40d1c679d58039e57602876d53dae3
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-31 19:22:31 +0400

    ingredients table created

--- cakey_dump.sql
+++ cakey_dump.sql
@@ -12,3 +12,10 @@ CREATE TABLE IF NOT EXISTS pastries (
     image_url VARCHAR(255),
     recipe JSON
 );
+
+-- Create the ingredients table
+CREATE TABLE IF NOT EXISTS ingredients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255)
+);
