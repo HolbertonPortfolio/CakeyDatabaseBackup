69c52463967660a246fde32528d6b334e738009e
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-31 19:21:01 +0400

    pastries table created

--- cakey_dump.sql
+++ cakey_dump.sql
@@ -3,3 +3,12 @@ CREATE DATABASE IF NOT EXISTS cakey;
 
 -- Switch to the database
 USE cakey;
+
+-- Create the pastries table
+CREATE TABLE IF NOT EXISTS pastries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255),
+    recipe JSON
+);
